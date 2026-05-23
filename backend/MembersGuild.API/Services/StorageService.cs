732136a05e2d9929c18d722b91f06a6ad5d9b95d
Nathan Forest <hoshie@gmail.com>
2026-05-23 11:27:46 +1000

    Phase 10: ACL Fix S3 Intergration

--- backend/MembersGuild.API/Services/StorageService.cs
+++ backend/MembersGuild.API/Services/StorageService.cs
@@ -7,15 +7,15 @@ namespace MembersGuild.API.Services;
 
 public class StorageService
 {
-    private readonly IAmazonS3    _s3;
-    private readonly string       _bucket;
-    private readonly string       _region;
+    private readonly IAmazonS3 _s3;
+    private readonly string _bucket;
+    private readonly string _region;
 
     public StorageService(IAmazonS3 s3, IConfiguration config)
     {
-        _s3     = s3;
+        _s3 = s3;
         _bucket = config["AWS:BucketName"] ?? "membersguild-assets";
-        _region = config["AWS:Region"]     ?? "ap-southeast-2";
+        _region = config["AWS:Region"] ?? "ap-southeast-2";
     }
 
     public string GetPublicUrl(string key)
@@ -26,11 +26,10 @@ public class StorageService
         using var stream = new MemoryStream(data);
         await _s3.PutObjectAsync(new PutObjectRequest
         {
-            BucketName  = _bucket,
-            Key         = key,
+            BucketName = _bucket,
+            Key = key,
             InputStream = stream,
             ContentType = contentType,
-            CannedACL   = S3CannedACL.PublicRead,
         });
         return GetPublicUrl(key);
     }
@@ -39,9 +38,9 @@ public class StorageService
     public async Task<(string Logo, string Icon192, string Icon512)> UploadLogoWithIconsAsync(
         byte[] imageData, string slug)
     {
-        var logoUrl   = await UploadAsync(imageData, $"clubs/{slug}/logo.png",       "image/png");
-        var icon192   = await ResizeAndUploadAsync(imageData, $"clubs/{slug}/icons/icon-192.png", 192);
-        var icon512   = await ResizeAndUploadAsync(imageData, $"clubs/{slug}/icons/icon-512.png", 512);
+        var logoUrl = await UploadAsync(imageData, $"clubs/{slug}/logo.png", "image/png");
+        var icon192 = await ResizeAndUploadAsync(imageData, $"clubs/{slug}/icons/icon-192.png", 192);
+        var icon512 = await ResizeAndUploadAsync(imageData, $"clubs/{slug}/icons/icon-512.png", 512);
 
         return (logoUrl, icon192, icon512);
     }
@@ -52,7 +51,7 @@ public class StorageService
 
     private async Task<string> ResizeAndUploadAsync(byte[] imageData, string key, int size)
     {
-        using var input  = new MemoryStream(imageData);
+        using var input = new MemoryStream(imageData);
         using var output = new MemoryStream();
 
         using (var image = await Image.LoadAsync(input))
